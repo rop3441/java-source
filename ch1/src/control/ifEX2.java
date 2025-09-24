56b90f55219490a382434f76de9858ad218211ae
전쇼릅 <tyfmq123@gmail.com>
2025-09-24 17:17:43 +0900

    조건문

---
+++ ch1/src/control/ifEX2.java
@@ -0,0 +1,41 @@
+package control;
+
+import java.util.Scanner;
+
+public class ifEX2 {
+    public static void main(String[] args) {
+        // 점수를 입력받아 A.B,C,D 등급 출력
+        // >=90 A, >=80 B, >=70 C, D
+
+        Scanner sc = new Scanner(System.in);
+        int jumsu = Integer.parseInt(sc.nextLine());
+
+        // if~eslse if~else
+
+        // if (jumsu >= 90) {
+        // System.out.println("등급 : A");
+        // } else if (jumsu >= 80) {
+        // System.out.println("등급 : B");
+        // } else if (jumsu >= 70) {
+        // System.out.println("등급 : C");
+        // }
+        // else {
+        // System.out.println("등급 : D");
+
+        // 변수 유효범위
+        // 선언 위치에 따라 사용 범위가 달라짐
+
+        char grade = ' ';
+        if (jumsu >= 90) {
+            grade = 'A';
+        } else if (jumsu >= 80) {
+            grade = 'B';
+        } else if (jumsu >= 70) {
+            grade = 'C';
+        } else {
+            grade = 'D';
+        }
+        System.out.println("등급 : " + grade);
+    }
+
+}
